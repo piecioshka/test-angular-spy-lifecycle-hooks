7dc603ef422b707b7c0b148aaf25b4b04fd99242
Piotr Kowalski <piecioshka@gmail.com>
2019-01-06 11:23:50 +0100

    feat(SpyLifecycleHooksDirective): generate class
    Generate class with CLI.
    Create methods to monitor each lifecycle hooks.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpyLifecycleHooksDirective } from './spy-lifecycle-hooks.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpyLifecycleHooksDirective
   ],
   imports: [
     BrowserModule
